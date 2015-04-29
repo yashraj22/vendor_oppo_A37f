3eace4e03bf6bd8fba7b86f1acac427a35d6813c
Michael Bestas <mikeioannina@gmail.com>
2015-04-29 17:52:13 +0300

    find7: Remove unused ES325 firmware
    Change-Id: Ia9d8e756b9d33773552c3b673ac05a47f76b8f4f

--- find7/find7-vendor-blobs.mk
+++ find7/find7-vendor-blobs.mk
@@ -54,8 +54,7 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/find7/proprietary/etc/firmware/cpp_firmware_v1_1_6.fw:system/etc/firmware/cpp_firmware_v1_1_6.fw \
     vendor/oppo/find7/proprietary/etc/firmware/cpp_firmware_v1_2_0.fw:system/etc/firmware/cpp_firmware_v1_2_0.fw \
     vendor/oppo/find7/proprietary/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
-    vendor/oppo/find7/proprietary/vendor/lib/libAKM8963.so:system/vendor/lib/libAKM8963.so \
-    vendor/oppo/find7/proprietary/etc/firmware/audience-es325-fw.bin:system/etc/firmware/audience-es325-fw.bin
+    vendor/oppo/find7/proprietary/vendor/lib/libAKM8963.so:system/vendor/lib/libAKM8963.so
 
 PRODUCT_COPY_FILES += \
     vendor/oppo/find7/proprietary/lib/hw/camera.vendor.msm8974.so:system/lib/hw/camera.vendor.msm8974.so \
