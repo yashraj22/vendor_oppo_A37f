a6dd11e03a0fb52b6cbe8962652d191b04bf28b4
Steve Kondik <steve@cyngn.com>
2016-06-05 00:43:25 -0700

    msm8974: Update blobs
    Change-Id: Iffb8b5b8f6b930cab0fc78fe575cc198cfbea4a2

--- msm8974-common/msm8974-common-vendor-blobs.mk
+++ msm8974-common/msm8974-common-vendor-blobs.mk
@@ -141,6 +141,7 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/liblqe.so:system/vendor/lib/liblqe.so \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libnetmgr.so:system/vendor/lib/libnetmgr.so \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libperipheral_client.so:system/vendor/lib/libperipheral_client.so \
+    vendor/oppo/msm8974-common/proprietary/vendor/lib/libQtiTether.so:system/vendor/lib/libQtiTether.so \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so \
     vendor/oppo/msm8974-common/proprietary/vendor/lib/libxml.so:system/vendor/lib/libxml.so \
     vendor/oppo/msm8974-common/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
--- msm8974-common/msm8974-common-vendor.mk
+++ msm8974-common/msm8974-common-vendor.mk
@@ -18,6 +18,7 @@ PRODUCT_PACKAGES += \
     CNEService \
     com.qualcomm.location \
     dpmserviceapp \
+    QtiTetherService \
     shutdownlistener
 
 PRODUCT_PACKAGES += \
