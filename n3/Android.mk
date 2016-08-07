bdce5f46931c535e260a7972c392b7c53799252b
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-08-07 18:33:43 +0300

    msm8974: Update for new extract utils
    Change-Id: I472846a748621c7774d13568c5b8b52111dd9360

--- n3/Android.mk
+++ n3/Android.mk
@@ -16,21 +16,17 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_VENDOR),oppo)
-ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
-ifneq ($(filter n3,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),n3)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
 LOCAL_MODULE_OWNER := oppo
 LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := .apk
 LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
-endif
-endif
 endif
